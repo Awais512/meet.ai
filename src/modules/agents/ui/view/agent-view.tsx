7704fedb1afdd476c3f156e7960e1285260241c9
awais512 <raza.awais25@gmail.com>
2025-06-09 20:56:22 +0100

    Added responsive dialog

--- src/modules/agents/ui/view/agent-view.tsx
+++ src/modules/agents/ui/view/agent-view.tsx
@@ -2,6 +2,7 @@
 
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
@@ -9,7 +10,19 @@ export const AgentView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      <ResponsiveDialog
+        title="Responsive Test"
+        description="Responsive test description"
+        open={false}
+        onOpenChange={() => {}}
+      >
+        Hello
+      </ResponsiveDialog>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
 };
 
 export const AgentsViewLoading = () => {
