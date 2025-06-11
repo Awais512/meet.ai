1495825075defa1e71eca9a54ddbea09ad18bfca
awais512 <raza.awais25@gmail.com>
2025-06-11 23:27:42 +0100

    Protect agent page

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -8,8 +8,19 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 import { ListHeader } from "@/modules/agents/ui/components/list-header";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Agents = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   const queryClient = getQueryClient();
 
   void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
